2e0d20a8aee93b51221b12673226ce7b25d7884d
Nui Narongwet <narongwet.m@gmail.com>
2024-04-03 21:32:58 +0700

    Upgrade to tmux 3.4-3

--- tmux/compat/htonll.c
+++ tmux/compat/htonll.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Nicholas Marriott <nicholas.marriott@gmail.com>
+ * Copyright (c) 2024 Nicholas Marriott <nicholas.marriott@gmail.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -15,40 +15,16 @@
  */
 
 #include <sys/types.h>
-#include <sys/socket.h>
-
-#include <stdio.h>
-
-#ifdef HAVE_UCRED_H
-#include <ucred.h>
-#endif
 
 #include "compat.h"
 
-int
-getpeereid(int s, uid_t *uid, gid_t *gid)
+uint64_t
+htonll(uint64_t v)
 {
-#ifdef HAVE_SO_PEERCRED
-	struct ucred	uc;
-	int		len = sizeof uc;
+    uint32_t b;
+    uint32_t t;
 
-	if (getsockopt(s, SOL_SOCKET, SO_PEERCRED, &uc, &len) == -1)
-		return (-1);
-	*uid = uc.uid;
-	*gid = uc.gid;
-	return (0);
-#elif defined(HAVE_GETPEERUCRED)
-        ucred_t *ucred = NULL;
-
-        if (getpeerucred(s, &ucred) == -1)
-                return (-1);
-        if ((*uid = ucred_geteuid(ucred)) == -1)
-                return (-1);
-        if ((*gid = ucred_getrgid(ucred)) == -1)
-                return (-1);
-        ucred_free(ucred);
-        return (0);
-#else
-	return (getuid());
-#endif
+    b = htonl (v & 0xffffffff);
+    t = htonl (v >> 32);
+    return ((uint64_t)b << 32 | t);
 }
